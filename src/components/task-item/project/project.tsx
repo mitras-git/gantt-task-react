e0676b3739adec20b238bfede6afd8b77773208c
mitras-git <myemail.aditya@gmail.com>
2025-07-27 15:38:30 +0300

    Fix bug in project bar rendering component

--- src/components/task-item/project/project.tsx
+++ src/components/task-item/project/project.tsx
@@ -2,76 +2,75 @@ import React from "react";
 import { TaskItemProps } from "../task-item";
 import styles from "./project.module.css";
 
-export const Project: React.FC<TaskItemProps> = ({ task }) => {
-  // const barColor = isSelected
-  //   ? task.styles.backgroundSelectedColor
-  //   : task.styles.backgroundColor;
-  // const processColor = isSelected
-  //   ? task.styles.progressSelectedColor
-  //   : task.styles.progressColor;
-  const projectWith = task.x2 - task.x1;
+export const Project: React.FC<TaskItemProps> = ({ task, isSelected }) => {
+  const barColor = isSelected
+    ? task.styles.backgroundSelectedColor
+    : task.styles.backgroundColor;
+  // FIX: Renamed 'processColor' to 'progressColor' for consistency and correctness
+  const progressColor = isSelected
+    ? task.styles.progressSelectedColor
+    : task.styles.progressColor;
+  // FIX: Renamed 'projectWith' to 'projectWidth' for clarity
+  const projectWidth = task.x2 - task.x1;
+
+  const projectLeftTriangle = [
+    task.x1,
+    task.y + task.height / 2 - 1,
+    task.x1,
+    task.y + task.height,
+    task.x1 + 15,
+    task.y + task.height / 2 - 1,
+  ].join(",");
+  const projectRightTriangle = [
+    task.x2,
+    task.y + task.height / 2 - 1,
+    task.x2,
+    task.y + task.height,
+    task.x2 - 15,
+    task.y + task.height / 2 - 1,
+  ].join(",");
 
   return (
     <g tabIndex={0} className={styles.projectWrapper}>
       <rect
-        fill="#e6a9a0"
+        fill={barColor}
         x={task.x1}
-        width={projectWith}
+        width={projectWidth} // Use corrected variable
         y={task.y}
         height={task.height}
         rx={task.barCornerRadius}
         ry={task.barCornerRadius}
         className={styles.projectBackground}
       />
-      {/* <rect
+      <rect
         x={task.progressX}
         width={task.progressWidth}
         y={task.y}
         height={task.height}
         ry={task.barCornerRadius}
         rx={task.barCornerRadius}
-        fill="#d67162"
-      /> */}
-      {task.progressWidth > task.barCornerRadius ? (
-              <React.Fragment>
-                <defs>
-                  <clipPath id={`barClipLeft-${task.x1}`}>
-                    <rect
-                      x={task.progressX}
-                      y={task.y}
-                      width={task.progressWidth}
-                      height={task.height}
-                      rx={task.barCornerRadius}
-                      ry={task.barCornerRadius}
-                    />
-                    <rect
-                      x={task.progressX + task.barCornerRadius}
-                      y={task.y}
-                      width={task.progressWidth - task.barCornerRadius}
-                      height={task.height}
-                    />
-                  </clipPath>
-                </defs>
-                <rect
-                  x={task.progressX}
-                  width={task.progressWidth}
-                  y={task.y}
-                  height={task.height}
-                  fill="#d67162"
-                  clipPath={`url(#barClipLeft-${task.x1})`}
-                />
-              </React.Fragment>
-            ) : (
-              <rect
-                x={task.progressX}
-                width={task.progressWidth}
-                y={task.y}
-                height={task.height}
-                rx={task.barCornerRadius}
-                ry={task.barCornerRadius}
-                fill="#d67162"
-              />
-            )}
+        fill={progressColor} // Use corrected variable
+      />
+      <rect
+        fill={barColor}
+        x={task.x1}
+        width={projectWidth} // Use corrected variable
+        y={task.y}
+        height={task.height / 2}
+        rx={task.barCornerRadius}
+        ry={task.barCornerRadius}
+        className={styles.projectTop}
+      />
+      <polygon
+        className={styles.projectTop}
+        points={projectLeftTriangle}
+        fill={barColor}
+      />
+      <polygon
+        className={styles.projectTop}
+        points={projectRightTriangle}
+        fill={barColor}
+      />
     </g>
   );
 };
